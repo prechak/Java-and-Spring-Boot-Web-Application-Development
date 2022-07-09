7546ad9d0e8997f273f51c0375f1eb587b5ac0e6
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-10 00:18:13 +0700

    update set cookie

--- Maven Project/firstweb/src/main/java/cookiesession/WriteCookie.java
+++ Maven Project/firstweb/src/main/java/cookiesession/WriteCookie.java
@@ -17,7 +17,16 @@ public class WriteCookie extends HttpServlet {
 		Cookie count = new Cookie("count", "0");	// "name", "value"
 		response.addCookie(count);
 		
-		Cookie man = new Cookie("Prasert", "79");
+		Cookie oldCount[] = request.getCookies();
+		for(Cookie cookie: oldCount) {
+			if(cookie.getName().equals("count")) {
+				System.out.println( cookie.getName() + "-" + cookie.getValue());
+				int a = Integer.parseInt(cookie.getValue()) + 1;
+				response.addCookie(new Cookie("count", String.valueOf(a) ) );		// replace old count
+			}
+		}
+		
+		Cookie man = new Cookie("Precha", "28");
 		response.addCookie(man);
 	}
 
