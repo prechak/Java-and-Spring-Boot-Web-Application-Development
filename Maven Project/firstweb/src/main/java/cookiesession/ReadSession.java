f9fffa96414b8cc3a01fe14bb2d2d86520f54bf7
Precha Krasaesup <78293149+prechak@users.noreply.github.com>
2022-07-11 00:06:33 +0700

    update test session

---
+++ Maven Project/firstweb/src/main/java/cookiesession/ReadSession.java
@@ -0,0 +1,21 @@
+package cookiesession;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ReadSession extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		session.getAttribute("c_user");
+		String user = (String)session.getAttribute("user");
+		System.out.println("Who am i :" + user);
+	}
+
+}
